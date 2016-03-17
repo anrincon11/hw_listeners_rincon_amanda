a784df127f124c7b975896236c5e5835ede909ad
Amanda Rincon <anrincon@ufl.edu>
2016-03-17 15:45:43 -0700

    add event listener for site heading

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/* Easter Egg One */
+var heading = document.getElementById( 'siteHeading' );
+
+heading.addEventListener( 'click', function() { //listens for a click on the siteHeading id element
+	heading.style.color = 'RoyalBlue'; //changes the color of the font to blue with a single click
+} );
