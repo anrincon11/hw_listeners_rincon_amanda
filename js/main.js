e3527e8054ec3a51da611b23743162aa8297eda8
Amanda Rincon <anrincon@ufl.edu>
2016-03-17 17:53:19 -0700

    add mouse event listener to aside element

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 /* Easter Egg One */
 var heading = document.getElementById( 'siteHeading' );
 
-heading.addEventListener( 'click', function() { //listens for a click on the siteHeading id element
+//listens for a click on the siteHeading id element
+heading.addEventListener( 'click', function() {
 	heading.style.color = 'RoyalBlue'; //changes the color of the font to blue with a single click
+} );
+
+/* Easter Egg Two */
+var hover = document.getElementById( 'sidebar' );
+
+//listens for when the mouse enters the sidebar area
+hover.addEventListener( 'mouseout', function() {
+	var myElement = document.createElement( 'article' );
+
+	myElement.innerHTML = "<p>The day before my mom's birthday!</p>";
+	document.getElementsByTagName( 'aside' )[ 0 ].appendChild( myElement );
 } );
