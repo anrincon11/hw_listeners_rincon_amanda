1282c7c3ee1fc836fc183b87d3cc15dff66f99a9
Amanda Rincon <anrincon@ufl.edu>
2016-03-17 18:13:39 -0700

    add event listener to the body element of the site

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ var heading = document.getElementById( 'siteHeading' );
 
 //listens for a click on the siteHeading id element
 heading.addEventListener( 'click', function() {
-	heading.style.color = 'RoyalBlue'; //changes the color of the font to blue with a single click
+	heading.style.color = 'RoyalBlue';
 } );
+
+//changes the color of the font to blue with a single click
 
 /* Easter Egg Two */
 var hover = document.getElementById( 'sidebar' );
@@ -16,3 +18,15 @@ hover.addEventListener( 'mouseout', function() {
 	myElement.innerHTML = "<p>The day before my mom's birthday!</p>";
 	document.getElementsByTagName( 'aside' )[ 0 ].appendChild( myElement );
 } );
+
+//adds a paragraph element to the bottom of the sidebar
+
+/* Easter Egg Three */
+var backgroundChange = document.getElementById( 'colors' );
+
+//listens for the press of a key on the body element
+backgroundChange.addEventListener( 'keydown', function() {
+	document.body.style.backgroundColor = 'Plum';
+} );
+
+//changes the background color of the page when a key is pressed on the keyboard
