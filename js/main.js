05949795184f2b0db70412c4539d4f74b3f3c807
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-29 21:21:27 -0700

    listen for  event

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var form = document.getElementById( 'contact-form' );
 
-form.addEventListener( 'click', function( event ) {
+form.addEventListener( 'submit', function( event ) {
 	event.preventDefault();
 	console.log( 'The firstName is: ' + document.getElementById( 'First' ).value );
 	console.log( 'The LastName is: ' + document.getElementById( 'Last' ).value );
